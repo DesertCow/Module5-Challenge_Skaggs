575b1d068fd615f60be8d85a1acc1a9633068b20
Clayton S <desertcow07@gmail.com>
2022-05-25 21:11:41 -0700

    Added Local storage | single line working | Save/Clear button logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,66 @@ var currentTime = moment();
 
 // Grab Display Fields in HTML
 var currentTimeEl = document.querySelector(".currentDayCL");
+var meetingTextInput = document.querySelector(".hourMeetingInfo");
+
+var saveButton = document.querySelector(".hourSave");
+
 
 console.log("TIME " + currentTimeEl);
+
+var planner = [
+  {
+    "hour": "9am",
+    "meetingNotes": "",
+    "saveButtonFlag": true,
+  },
+  {
+
+  },
+]
+
+
+
+var objCount = 0;
+
+
+// ################# Button Listeners #################
+
+saveButton.addEventListener("click", function (clickEvent) {
+
+  if (planner[objCount].saveButtonFlag) {
+    meetingTextInput.readOnly = true;
+    meetingTextInput.style.backgroundColor = '#EBB938';
+
+    changeToClearButton(objCount);
+
+    saveMeetingInfo(clickEvent);
+  } else {
+    meetingTextInput.readOnly = false;
+    changeToSaveButton(objCount);
+    meetingTextInput.style.backgroundColor = '#FAEDCD';
+  }
+
+
+  clickEvent.preventDefault();
+});
+
+// ################# Mouse Over Listeners #################
+
+saveButton.addEventListener('mouseover', function handleMouseOver() {
+
+  if (planner[objCount].saveButtonFlag) {
+    saveButton.style.backgroundColor = '#515B2F';
+  } else {
+    saveButton.style.backgroundColor = '#E5D2FC';
+  }
+
+});
+
+saveButton.addEventListener('mouseout', function handleMouseOut() {
+  saveButton.style.backgroundColor = '#CCD5AE';
+});
+
 
 
 // ==================== Function Decleration  =====================
@@ -24,6 +82,47 @@ function init() {
   currentTimeEl.textContent = currentTime.format("MMM Do YYYY");
   console.log("Init Complete!");
 }
+
+// ################# FUNC_NAME #################
+
+function saveMeetingInfo(event) {
+
+
+  //console.log("Save Meeting DATA = " + meetingTextInput.value)
+  //console.log("Save Meeting DATA Stringify = " + JSON.stringify(meetingTextInput.value))
+  localStorage.setItem("meetingInfo", meetingTextInput.value);
+  //localStorage.setItem("meetingInfo", JSON.stringify(meetingTextInput.value));
+
+}
+
+
+// ################# changeToClearButton #################
+function changeToClearButton(Count) {
+  planner[Count].saveButtonFlag = false;
+  saveButton.textContent = "Clear";
+  saveButton.style.backgroundColor = '#A462F4';
+
+  console.log("Flag = " + planner[Count].saveButtonFlag)
+}
+
+// ################# changeToSaveButton #################
+function changeToSaveButton(Count) {
+  planner[Count].saveButtonFlag = true;
+  saveButton.textContent = "Save";
+  saveButton.style.backgroundColor = '#515B2F';
+
+  console.log("Flag = " + planner[Count].saveButtonFlag)
+}
+
+// ################# FUNC_NAME #################
+
+
+// ################# FUNC_NAME #################
+
+
+
+// ################# FUNC_NAME #################
+
 
 // ################# FUNC_NAME #################
 
